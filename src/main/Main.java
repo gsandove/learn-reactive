e809e3f0fb8dd9cb0bc27520b3d999216986aa94
George Sandoval <george.sandoval.estela@gmail.com>
2024-01-30 19:42:27 -0500

    implementacion de 2 hilos

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,6 +7,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Main");
 		RHilo hilo = new RHilo(1);
+		RHilo hilo2 = new RHilo(2);
 		hilo.run();
+		hilo2.run();
 	}
 }
