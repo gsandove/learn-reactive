802db162d5bbbc4b6f670b9cc1fa831c2254198e
George Sandoval <george.sandoval.estela@gmail.com>
2024-01-30 19:35:14 -0500

    implement hilo

---
+++ src/threads/RHilo.java
@@ -0,0 +1,17 @@
+package threads;
+
+public class RHilo implements Runnable{
+	
+	private int id;
+	
+	public RHilo(int id) {
+		this.id = id;
+	}
+	
+	@Override
+	public void run() {
+		for (int i = 0; i < 100 ; i++) {
+			System.out.println("[R] Ejecutándose hilo de id --> " + id);
+		}
+	}
+}
